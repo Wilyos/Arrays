20584b62a4a96db499d8d41b64ed268259bea419
wilyos <wilyos99@gmail.com>
2022-04-08 20:44:36 -0500

    Segunda clase de arreglos

---
+++ src/co/edu/cesde/Student.java
@@ -0,0 +1,33 @@
+package co.edu.cesde;
+
+public class Student {
+
+   private String photoUrl;
+   private String identification;
+   private String name;
+
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public String getIdentification() {
+        return identification;
+    }
+
+    public void setIdentification(String identification) {
+        this.identification = identification;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
