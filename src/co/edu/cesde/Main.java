1272ee4e358918c9dd3dc2c1c804e013626d19c6
wilyos <wilyos99@gmail.com>
2022-04-22 20:27:44 -0500

    tercera clase de arreglos
    nueva forma de arreglos
    mostrar lista
    buscar
    eliminar yactualizar

--- src/co/edu/cesde/Main.java
+++ src/co/edu/cesde/Main.java
@@ -1,27 +1,74 @@
 package co.edu.cesde;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	Scanner inputData = new Scanner(System.in);
-    int numStudents=2;
 
-        Student[] students = new Student[numStudents];
-        for (int i = 0; i <students.length; i++) {
+        Scanner inputData = new Scanner(System.in);
+        List<Student> students = new ArrayList<>();
+        String answer="si";
+
+        while(answer.equals("si")) {
+
 
             Student person = new Student();
             System.out.println("ingrese el nombre: ");
             person.setName(inputData.nextLine());
             System.out.println("ingrese la identificacion: ");
             person.setIdentification(inputData.nextLine());
+            students.add(person);
+            System.out.println("¿Desea introducir otro estudiante? SI - NO");
+            String stop = inputData.nextLine();
 
-            students[i] = person;
+            if(stop.equalsIgnoreCase("no")){
+                answer= "no"; //break; es otra forma de pararlo
+            }
         }
-        for (int i = 0; i <numStudents; i++) {
+        //mostrar lista
+        for (int i = 0; i <students.size() ; i++) {
 
-            System.out.println(students[i].getName());
-            System.out.println(students[i].getIdentification());
+            System.out.println("Nombre: "+ students.get(i).getName());
         }
+        //buscar estudiante
+        System.out.println("Ingrese el nombredel estudiante a buscar");
+        String name = inputData.nextLine();
+        for (int i = 0; i < students.size() ; i++) {
+            if(students.get(i).getName().equalsIgnoreCase(name)){
+                System.out.println("El estudiante: "+name+" su identificacion es: "+students.get(i).getIdentification());
+                break;
+            }
+
         }
+        // eliminar estudiante
+        System.out.println("Ingrese la identificacion del estudiante que desea eliminar: ");
+        String delete = inputData.nextLine();
+        for (int i = 0; i < students.size() ; i++) {
+            if(students.get(i).getIdentification().equalsIgnoreCase(delete)){
+                students.remove(i);
+                break;
+            }
+
+        }
+
+        //actualizar
+        System.out.println("Ingrese la identificacion del estudiante que desea actualizar: ");
+        String actualizar = inputData.nextLine();
+        for (int i = 0; i < students.size() ; i++) {
+            if(students.get(i).getIdentification().equalsIgnoreCase(actualizar)){
+                Student student = students.get(i);
+                System.out.println("ingrese la nueva id: ");
+                String ident = inputData.nextLine();
+                student.setIdentification(ident);
+                students.set(i,student);
+                break;
+            }
+
+        }
+
+
+
     }
+}
 
